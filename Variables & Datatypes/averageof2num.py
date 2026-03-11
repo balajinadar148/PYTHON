d0dc969e719dc3c33729a2991b68d0d6d3a4fc91
Balaji Nadar <balajinadar148@gmail.com>
2026-03-11 12:09:18 +0530

    initial commit

---
+++ Variables & Datatypes/averageof2num.py
@@ -0,0 +1,4 @@
+a = int(input("Enter the 1st Number:"))
+b = int(input("Enter the 2nd Number: "))
+
+print ("The Average of 2 Numbers is", (a+b)/2)
---
+++ Variables & Datatypes/greaterthanornot.py
@@ -0,0 +1,4 @@
+a = int(input("Enter the number 1: "))
+b = int(input("Enter the number 2: "))
+
+print ("Is number 1 greater than number 2?", a > b)
