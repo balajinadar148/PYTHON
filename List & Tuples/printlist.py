fb17ee9116e71011a431db6b0bd7828b8136bedb
Balaji Nadar <balajinadar148@gmail.com>
2026-03-14 22:20:13 +0530

    New Topic Begins

---
+++ List & Tuples/printlist.py
@@ -0,0 +1,15 @@
+Fruits = []
+
+f1= input("Enter the name of the first fruit: ")
+Fruits.append(f1)
+f2= input("Enter the name of the first fruit: ")
+Fruits.append(f2)
+f3= input("Enter the name of the first fruit: ")
+Fruits.append(f3)
+f4= input("Enter the name of the first fruit: ")
+Fruits.append(f4)
+f5= input("Enter the name of the first fruit: ")
+Fruits.append(f5)
+
+print("The fruits you entered are: ")
+print(Fruits)
